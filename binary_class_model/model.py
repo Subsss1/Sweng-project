07bd45de6201b5989a87afa57f546c1a04f4247c
ReRi1600 <rah135.rm@gmail.com>
2024-02-20 23:03:51 +0000

    beginning to have model train on data; encountered issue passing string data

--- binary_class_model/model.py
+++ binary_class_model/model.py
@@ -1,39 +1,47 @@
 from sklearn.utils import Bunch
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 import numpy as np
 import csv
 
 
-def load_dummy_data():
-    with open(r'dummy_data.csv') as csv_file:
+def load_data():
+    with open(r'human_update_EXAMPLE.csv') as csv_file:
         data_reader = csv.reader(csv_file)
-        #print(data_reader)
-        properties = next(data_reader)[:7]
-        #print(properties)
+        properties = next(data_reader)[:9]
+        print(properties)
         data = []
         author = []
         for row in data_reader:
-            #print(row)
-            stats = row[:7]
-            #print(features)
-            label = row[7] #index to whichever column indicates machine/human generated
-            #print(label)
-            data.append([float(num) for num in stats])
-            #print(data)
-            author.append(float(label))
-            #print(author)
+            stats = row[:9]
+            label = row[9] #index to whichever column indicates machine/human generated
+            data.append([num for num in stats])
+            author.append(int(label))
 
-        data = np.array(data)
-        #print(data)
-        author = np.array(author)
-        #print(author)
+        #data = np.array(data)
+        #author = np.array(author)
     return Bunch(data=data, author=author, properties=properties)
 
 
-dummy = load_dummy_data()
+dummy = load_data()
 prop = dummy['properties']
 print("prop: ", prop)
 data = dummy['data']
-print("data: ", data)
 auth = dummy['author']
-print("auth: ", auth)
+print("auth: ", auth[:10])
+
+training_data, testing_data, training_labels,  testing_labels = train_test_split(data, auth, stratify=auth,
+                                                                                test_size=0.2, random_state=1)
+print(training_data[:9])
+print(testing_data[:9])
+print(training_labels[:9])
+print(testing_labels[:9])
+
+gauss = GaussianNB()
+model = gauss.fit(training_data,training_labels)
+test_results = gauss.predict(testing_data)
+print(test_results)
+
+score = accuracy_score(testing_labels,test_results)
+print(score)
