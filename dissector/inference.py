d1a8dfc0f2ac1d3439c587289cc9d57360753e8b
ayndqy <ayndqy@proton.me>
2024-02-21 21:54:08 +0000

    Add caching to the dissector - Prettify subtree output

--- dissector/inference.py
+++ dissector/inference.py
@@ -1,11 +1,13 @@
 # Script that runs model
 import sys
+import time
 import random
 
 # Get arguments
 arguments = sys.argv[1:]
 
 # Some magical stuff with model (run inference using arguments)
+time.sleep(1) # Simulate heavy work
 result = random.uniform(0, 1)
 
 # Return result
