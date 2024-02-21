4ac6c8a6454ef02710883918621bd49d4a0226cb
ayndqy <ayndqy@proton.me>
2024-02-21 23:06:21 +0000

    Prettify code

--- dissector/ismachine.lua
+++ dissector/ismachine.lua
@@ -6,48 +6,45 @@ ismachine_protocol.fields.probability = ProtoField.string("ismachine.probability
 cached_result = {}
 
 function ismachine_protocol.dissector(buffer, pinfo, tree)
-  -- Set name in "Protocol" column
+  -- Set name in protocol column
   pinfo.cols.protocol = ismachine_protocol.name
-
-  -- Add subtree in packet info
-  local subtree = tree:add(ismachine_protocol, buffer(), "IsMachine Protocol Data")
 
   -- Get packet info
   local args = {}
 
-  -- Execute model or get result from cache
+  -- Get inference result
   local result = cached_result[pinfo.number]
   if not result then
     result = infer(args)
     cached_result[pinfo.number] = result
   end
 
-  -- Format fields
+  -- Get fields
   local machine_generated_probability = tonumber(result)
   local generated_by = machine_generated_probability >= 0.5 and "Machine" or "Human"
   local probability = machine_generated_probability >= 0.5 and machine_generated_probability or 1 - machine_generated_probability
 
-  -- Add fields
+  -- Add subtree
+  local subtree = tree:add(ismachine_protocol, buffer(), "IsMachine Protocol Data")
   subtree:add(ismachine_protocol.fields.generated_by, generated_by)
   subtree:add(ismachine_protocol.fields.probability, probability)
 end
 
-local udp_table = DissectorTable.get("udp.port")
-udp_table:add(1900, ismachine_protocol)
+DissectorTable.get("tcp.port"):add(80, ismachine_protocol)
+DissectorTable.get("udp.port"):add(1900, ismachine_protocol)
 
------ Helpers -----
+-- Helpers
 function infer(args)
-  local command = "python3 ~/.local/lib/wireshark/plugins/inference.py " .. table.concat(args, " ")
-  local handle = io.popen(command,"r")
+  local command = "python3 ~/.local/lib/wireshark/plugins/inference.py"
+  local command_args = " " .. table.concat(args, " ")
+  local handle = io.popen(command .. command_args, "r")
   local output = handle:read("*a")
   handle:close()
-
   return remove_last_line(output)
 end
 
 function remove_last_line(str)
   local last_line_index = str:find("\n[^\n]*$")
-
   if last_line_index then
     return str:sub(1, last_line_index - 1)
   else
