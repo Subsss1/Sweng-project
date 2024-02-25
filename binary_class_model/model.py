c16fdf11129050621848068c0f15549a3c03d1ac
ReRi1600 <rah135.rm@gmail.com>
2024-02-25 22:42:26 +0000

    First working build of model

--- binary_class_model/model.py
+++ binary_class_model/model.py
@@ -2,34 +2,65 @@ from sklearn.utils import Bunch
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
-import numpy as np
 import csv
+import re
 
 
 def load_data():
     with open(r'human_update_EXAMPLE.csv') as csv_file:
         data_reader = csv.reader(csv_file)
         properties = next(data_reader)[:9]
-        print(properties)
-        data = []
+        #protocols = []
+        #print(properties)
+        data = [1]
         author = []
+        with open("regex.txt","r") as f:
+            regexes = [line.split("~") for line in f.read().split("\n")]
         for row in data_reader:
-            stats = row[:9]
-            label = row[9] #index to whichever column indicates machine/human generated
-            data.append([num for num in stats])
-            author.append(int(label))
+            if row[4] == 'TCP':
+                row[4] = 0
+            elif row[4] == 'UDP':
+                row[4] = 1
+            elif row[4] == 'TLSv1.2':
+                row[4] = 2
+            elif row[4] == 'DHCP':
+                row[4] = 3
+            elif row[4] == 'DNS':
+                row[4] = 4
+            elif row[4] == 'TLSv1.3':
+                row[4] = 5
+            elif row[4] == 'HTTP':
+                row[4] = 6
+            elif row[4] == 'OCSP':
+                row[4] = 7
+            elif row[4] == 'SSDP':
+                row[4] = 8
+            else:
+                row[4] = 9
 
-        #data = np.array(data)
-        #author = np.array(author)
+
+            for regex, category in regexes:
+                if re.match(rf"{regex}", row[6]):
+                    row[6] = category
+                    break
+
+            #if(not row[6] in protocols):
+            #    protocols.append(row[6])
+
+            #print(row[0], " : ", row[6])
+            label = row[9] #index to whichever column indicates machine/human generated
+            stats = row[:9]
+            data.append([float(num) for num in stats])
+            author.append(int(label))
+        #print(protocols)
     return Bunch(data=data, author=author, properties=properties)
 
 
 dummy = load_data()
 prop = dummy['properties']
-print("prop: ", prop)
 data = dummy['data']
+data.remove(1)
 auth = dummy['author']
-print("auth: ", auth[:10])
 
 training_data, testing_data, training_labels,  testing_labels = train_test_split(data, auth, stratify=auth,
                                                                                 test_size=0.2, random_state=1)
@@ -44,4 +75,4 @@ test_results = gauss.predict(testing_data)
 print(test_results)
 
 score = accuracy_score(testing_labels,test_results)
-print(score)
+print("score: ", score)
