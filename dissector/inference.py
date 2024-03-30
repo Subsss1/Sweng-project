05981115a5b3b7d689cf116bfa2cf10d405ef350
ayndqy <ayndqy@proton.me>
2024-03-30 19:52:09 +0000

    Change parameters

--- dissector/inference.py
+++ dissector/inference.py
@@ -3,20 +3,17 @@ import ipaddress
 import joblib
 
 # Get arguments
-arguments = sys.argv[1:] # [ number, timestamp, ipv, src, dst, src_port, dst_port, proto, len ]
+arguments = sys.argv[1:] # [ src_port, dst_port, proto, delta_time, len, avg_len ]
 
 # Format input data
-number = int(arguments[0])
-timestamp = float(arguments[1])
-ipv = int(arguments[2])
-src = int(ipaddress.IPv4Address(arguments[3]))
-dst = int(ipaddress.IPv4Address(arguments[4]))
-src_port = int(arguments[5])
-dst_port = int(arguments[6])
-proto = int(arguments[7])
-length = int(arguments[8])
+src_port = int(arguments[0])
+dst_port = int(arguments[1])
+proto = int(arguments[2])
+delta_time = float(arguments[3])
+length = int(arguments[4])
+avg_len = float(arguments[5])
 
-input_data = [[number, ipv, timestamp, src, dst, src_port, dst_port, length, proto]]
+input_data = [[ src_port, dst_port, proto, delta_time, length, avg_len ]]
 
 # Run model
 model = joblib.load('model.pkl')
