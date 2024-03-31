43910e4900bc7911b919a87c07608bfbb06dc1f7
ayndqy <ayndqy@proton.me>
2024-03-31 06:54:43 +0100

    Model improvement

---
+++ Makefile
@@ -0,0 +1,2 @@
+capture2csv:
+	python3 ./convert/capture2dataset.py $(capture) $(labels) $(output)
---
+++ model/model_SGDC.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import joblib
+
+# Load the dataset
+datasets = [
+  pd.read_csv('dataset1.csv'),
+  pd.read_csv('dataset2.csv'),
+  pd.read_csv('dataset3.csv'),
+  pd.read_csv('dataset4.csv'),
+  pd.read_csv('dataset5.csv'),
+]
+data = pd.concat(datasets, ignore_index=True)
+
+# Drop unnecessary columns
+data = data.drop(['number'], axis=1)
+
+print(data)
+
+# Encode the 'human' column using LabelEncoder
+label_encoder = LabelEncoder()
+data['human'] = label_encoder.fit_transform(data['human'])
+
+# Split the data into features and target variable
+X = data.drop('human', axis=1)
+y = data['human']
+
+# Split the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = SGDClassifier(
+    loss='log_loss',
+    max_iter=1000,
+    tol=1e-3,
+    alpha=0.0001,
+    penalty='l2',
+    learning_rate='optimal',
+    eta0=0.1,
+    power_t=0.5
+)
+
+model.fit(X_train, y_train)
+
+y_prob = model.predict_proba(X_test)
+y_pred = (y_prob[:, 1] >=  0.5).astype(int)
+
+# Evaluate the model's performance on the test set
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy on test set: {accuracy}")
+
+joblib.dump(model, './model.pkl')
+print(f"Model dumped to ./model.pkl")
